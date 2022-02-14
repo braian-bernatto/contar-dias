a1a33fe77bc5fc25f33381b81d7b0133d61c2529
Bernatto <braianbernatto@hotmail.com>
2022-02-14 09:01:17 -0300

    footer updated v2

--- pages/index.js
+++ pages/index.js
@@ -41,9 +41,7 @@ export default function Home({ fechas }) {
       </div>
 
       <footer className='flex flex-wrap justify-center items-center mt-5'>
-        <span className='text-xs text-gray-800 font-bold'>
-          by Bernatto
-        </span>
+        <span className='text-xs text-gray-500 font-bold'>by Bernatto</span>
       </footer>
     </div>
   )
