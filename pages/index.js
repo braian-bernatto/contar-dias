5247a296e44c32cab51177dea1d1089bb683443b
Bernatto <braianbernatto@hotmail.com>
2022-02-20 19:40:50 -0300

    copyright added

--- pages/index.js
+++ pages/index.js
@@ -59,7 +59,9 @@ export default function Home() {
       </div>
 
       <footer className='flex flex-wrap justify-center items-center mt-5'>
-        <span className='text-xs text-gray-500 font-bold'>by Bernatto</span>
+        <span className='text-xs text-gray-500 font-bold'>
+          &copy; {new Date().getFullYear()} - Bernatto
+        </span>
       </footer>
     </div>
   )
