d356cfac9c263bf325f79bd2eb9c49aed8936458
bernatto_uec <braian5353@gmail.com>
2024-01-04 11:21:27 -0300

    admin link added

--- pages/index.js
+++ pages/index.js
@@ -62,10 +62,29 @@ export default function Home({ fechas }) {
         </main>
       </div>
 
-      <footer className='flex flex-wrap justify-center items-center mt-5'>
+      <footer className='flex flex-wrap justify-center items-center mt-5 gap-4 pb-5'>
         <span className='text-xs text-gray-500 font-bold'>
           &copy; {new Date().getFullYear()} - Bernatto
         </span>
+        <span className='text-gray-400'>{' | '}</span>
+        <a
+          href='https://strapi-dias.bernatto.xyz/admin'
+          rel='noreferrer'
+          target='_blank'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='w-6 h-6 text-gray-500'>
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75'
+            />
+          </svg>
+        </a>
       </footer>
     </div>
   )
