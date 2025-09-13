a90df0f3cfe18c23e8803e4de0ee67dd871f5682
braian-bernatto <braian5353@gmail.com>
2025-09-13 13:44:07 -0300

    fix: remove hardcoded variables

--- next.config.js
+++ next.config.js
@@ -1,14 +1,3 @@
-// module.exports = {
-//   reactStrictMode: true,
-//   env: {
-//     backendURL: 'http://localhost:1337',
-//     frontendURL: 'http://localhost:3000'
-//   }
-// }
 module.exports = {
   reactStrictMode: true,
-  env: {
-    backendURL: 'https://strapi-dias.bernatto.xyz',
-    frontendURL: 'http://localhost:3000'
-  }
 }
